88c5994085561b7ea46283aa22b1554614d01d1a
AvishkaSooriyapperuma <adsooriyapperuma@students.nsbm.lk>
2021-01-26 22:25:57 +0530

    navbar is finished

--- src/Components/NavBar/Nav.jsx
+++ src/Components/NavBar/Nav.jsx
@@ -1,26 +1,55 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import {
+MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse,MDBContainer} from "mdbreact";
+import { BrowserRouter as Router } from 'react-router-dom';
+import Foss from './Assets/Logo/Foss.png';
 
-export default class Nav extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="#">Logo</a>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item active">
-                        <a class="nav-link" href="#">Dashboard <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Services <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">About <span class="sr-only">(current)</span></a>
-                        </li>
-                    </ul>
-                    </div>
-                </nav>
-            </React.Fragment>
-        )
-    }
+
+
+class NavbarPage extends Component {
+state = {
+  isOpen: false
+};
+
+toggleCollapse = () => {
+  this.setState({ isOpen: !this.state.isOpen });
 }
+
+render() {
+  return (
+    <Router>
+        
+      <MDBNavbar className="bg-white" dark expand="md">
+      <MDBContainer>
+        <MDBNavbarBrand>
+        <img src={Foss} className="img-fluid" alt="logo"  style={{
+                  width: "45px",
+                }} />
+        </MDBNavbarBrand>
+        <MDBNavbarToggler className="bg-light" onClick={this.toggleCollapse} />
+        <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+          <MDBNavbarNav right>
+            <MDBNavItem active>
+              <MDBNavLink className="text-dark" to="#!">Home</MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink className="text-dark" to="#!">Features</MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink className="text-dark" to="#!">Pricing</MDBNavLink>
+            </MDBNavItem>   
+          </MDBNavbarNav>
+     
+        </MDBCollapse>
+        </MDBContainer>
+      </MDBNavbar>
+     
+      <MDBContainer className="my-5">
+
+      </MDBContainer>
+    </Router>
+    );
+  }
+}
+
+export default NavbarPage;
