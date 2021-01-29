70be7070940a4cf186e964894259e7b90237f345
prineth <prinethferandox@gmail.com>
2021-01-30 05:00:21 +0530

    align the components
    it's better

--- src/Components/pages/about/content.jsx
+++ src/Components/pages/about/content.jsx
@@ -3,8 +3,13 @@ import React from "react";
 function Content() {
   return (
     <>
-      <h2 className="container">About page</h2>
-      <h6 className="container">
+      <div className="container">
+        <h2 className="font-size-lg-3 font-size-sm-2 font-size-1 mb-4">
+          About page
+        </h2>
+      </div>
+
+      <h6 className="container mb-4">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi impedit
         harum molestiae perferendis vitae rerum? Voluptas velit aliquam
         exercitationem quia rem id natus nam odit inventore? Ad architecto alias
--- src/Components/pages/about/formAndSlide.jsx
+++ src/Components/pages/about/formAndSlide.jsx
@@ -3,95 +3,122 @@ import React from "react";
 function FormAndSlide() {
   return (
     <>
-      <div className="row">
-        <div className="col">
-          <form className="container">
-            <div className="mb-3">
-              <label htmlFor="exampleInputEmail1" className="form-label">
+      <div className="container mt-4">
+        <div className="row">
+          <div className="col-md-5">
+            <h3 className="container">Contact</h3>
+            <form>
+              <div className="mt-4">
+                {/*<label htmlFor="exampleInputEmail1" className="form-label">
                 Email address
-              </label>
-              <input
-                type="email"
-                className="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-              />
-              <div id="emailHelp" className="form-text">
-                We'll never share your email with anyone else.
+                </label>*/}
+                <input
+                  type="email"
+                  className="form-control mt-4"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="Email address"
+                />
+                <div id="emailHelp" className="form-text">
+                  <small>We'll never share your email with anyone else.</small>
+                </div>
               </div>
-            </div>
-            <div className="mb-3">
-              <label htmlFor="exampleInputPassword1" className="form-label">
-                Subject
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="exampleInputPassword1"
-              />
-            </div>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  className="form-control "
+                  id="exampleInputPassword1"
+                  placeholder="Subject"
+                />
+              </div>
 
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </form>
-        </div>
-        <div className="col">
-          <div
-            id="carouselExampleIndicators"
-            className="carousel slide"
-            data-ride="carousel"
-          >
-            <ol className="carousel-indicators">
-              <li
-                data-target="#carouselExampleIndicators"
-                data-slide-to={0}
-                className="active"
+              <textarea
+                className="form-control"
+                id="exampleFormControlTextarea1"
+                rows={3}
+                defaultValue={""}
+                placeholder="Massage"
               />
-              <li data-target="#carouselExampleIndicators" data-slide-to={1} />
-              <li data-target="#carouselExampleIndicators" data-slide-to={2} />
-            </ol>
-            <div className="carousel-inner">
-              <div className="carousel-item active">
-                <img
-                  className="d-block w-100"
-                  src="https://pbs.twimg.com/media/ESrS0FwUwAQ8V59.jpg"
-                  alt="First slide"
+              <button
+                type="submit"
+                className="btn btn-primary rounded-pill mt-4"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+
+          <div className="col-md-1"></div>
+
+          <div className="col-md-6">
+            <div
+              id="carouselExampleIndicators"
+              className="carousel slide"
+              data-ride="carousel"
+            >
+              <ol className="carousel-indicators">
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to={0}
+                  className="active"
                 />
-              </div>
-              <div className="carousel-item">
-                <img
-                  className="d-block w-100"
-                  src="https://www.nsbm.ac.lk/wp-content/uploads/2019/09/01-7.jpg"
-                  alt="Second slide"
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to={1}
                 />
-              </div>
-              <div className="carousel-item">
-                <img
-                  className="d-block w-100"
-                  src="https://media-exp1.licdn.com/dms/image/C561BAQGjtb1ISMx83w/company-background_10000/0/1601264433225?e=2159024400&v=beta&t=R7fpNuvqTezg7y8momjKVp6JayY7C6r4DONl-txmDf4"
-                  alt="Third slide"
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to={2}
                 />
+              </ol>
+              <div className="carousel-inner rounded">
+                <div className="carousel-item active">
+                  <img
+                    className="d-block w-100"
+                    src="https://pbs.twimg.com/media/ESrS0FwUwAQ8V59.jpg"
+                    alt="First slide"
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    className="d-block w-100"
+                    src="https://www.nsbm.ac.lk/wp-content/uploads/2019/09/01-7.jpg"
+                    alt="Second slide"
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    className="d-block w-100"
+                    src="https://media-exp1.licdn.com/dms/image/C561BAQGjtb1ISMx83w/company-background_10000/0/1601264433225?e=2159024400&v=beta&t=R7fpNuvqTezg7y8momjKVp6JayY7C6r4DONl-txmDf4"
+                    alt="Third slide"
+                  />
+                </div>
               </div>
+              <a
+                className="carousel-control-prev"
+                href="#carouselExampleIndicators"
+                role="button"
+                data-slide="prev"
+              >
+                <span
+                  className="carousel-control-prev-icon"
+                  aria-hidden="true"
+                />
+                <span className="sr-only">Previous</span>
+              </a>
+              <a
+                className="carousel-control-next"
+                href="#carouselExampleIndicators"
+                role="button"
+                data-slide="next"
+              >
+                <span
+                  className="carousel-control-next-icon"
+                  aria-hidden="true"
+                />
+                <span className="sr-only">Next</span>
+              </a>
             </div>
-            <a
-              className="carousel-control-prev"
-              href="#carouselExampleIndicators"
-              role="button"
-              data-slide="prev"
-            >
-              <span className="carousel-control-prev-icon" aria-hidden="true" />
-              <span className="sr-only">Previous</span>
-            </a>
-            <a
-              className="carousel-control-next"
-              href="#carouselExampleIndicators"
-              role="button"
-              data-slide="next"
-            >
-              <span className="carousel-control-next-icon" aria-hidden="true" />
-              <span className="sr-only">Next</span>
-            </a>
           </div>
         </div>
       </div>
