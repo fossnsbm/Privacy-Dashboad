91ec8b689e8aa75e7febce25846ad3b7ccc08835
prineth <prinethferandox@gmail.com>
2021-01-30 02:36:07 +0530

    Add the about to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Card from './Components/Card/Card';
-import Nav from './Components/NavBar/Nav';
-import Content from './Components/Content/Content';
+import Card from "./Components/Card/Card";
+import Nav from "./Components/NavBar/Nav";
+import Content from "./Components/Content/Content";
+import About from "./Components/pages/about";
 
 export default class App extends Component {
   render() {
@@ -11,7 +12,8 @@ export default class App extends Component {
         <Nav />
         <Content />
         <Card />
+        <About />
       </div>
-    )
+    );
   }
 }
