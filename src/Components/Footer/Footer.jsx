5c5f92028915d0c342bc9f413f4e4096dfb8abb7
prineth <prinethferandox@gmail.com>
2021-02-06 04:28:46 +0530

    footer css path

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import "../Footer/Footer.css";
+import "./Footer.css";
 import { MDBCol, MDBContainer, MDBRow, MDBFooter, MDBIcon } from "mdbreact";
 
 const Footer = () => {
   return (
     <MDBFooter
+      id="footer"
       color=""
       className="font-small pt-3 pb-3 mt-4 bg-light text-dark"
     >
